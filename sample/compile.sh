630f9d3307c605c6a49422a08ddbd84b0ed79e2a
muupan <muupan@gmail.com>
2014-07-06 17:18:50 +0900

    compile.sh supports linux.

--- sample/compile.sh
+++ sample/compile.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 rm -rf tmp/*
-$CXX sample.cpp -g -std=c++11 -I../include -L../lib -lggpe -lboost_system-mt -lboost_timer-mt -lodbc -lgmp -lmysqlclient -lboost_filesystem-mt -lboost_regex-mt -lreadline -lYap -lglog
+if [ "`uname`" = "Linux" ]; then
+  BOOST_SUFFIX=
+else
+  BOOST_SUFFIX=-mt
+fi
+$CXX sample.cpp -g -std=c++11 -I../include -L../lib -lggpe -lboost_system$BOOST_SUFFIX -lboost_timer$BOOST_SUFFIX -lboost_filesystem$BOOST_SUFFIX -lboost_regex$BOOST_SUFFIX -lYap -lreadline -lglog -ldl
