c860c7ef8d76ea5c816f898b745789995219c441
muupan <muupan@gmail.com>
2014-07-14 04:20:49 +0900

    Add conj_to_list/2.

--- list_to_conj.pl
+++ list_to_conj.pl
@@ -1,3 +1,8 @@
 list_to_conj([H], H) :- !.
 list_to_conj([H | T], ','(H, Conj)) :-
     list_to_conj(T, Conj).
+
+conj_to_list(','(H, Conj), [H | T]) :-
+      !, conj_to_list(Conj, T).
+conj_to_list(H, [H]).
+
